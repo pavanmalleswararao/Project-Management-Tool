836cf702580e1228215ec1a31b41dd83a80f69f0
pavanmalleswararao <128814191+pavanmalleswararao@users.noreply.github.com>
2023-11-13 08:46:45 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer id="footer">
+  <div id="slideContainer">
+    <div id="slide1">
+      <div id="desc">
+        <div id="title">PMT (Project Management Tool)</div>
+        <div id="details">
+          Project Management TOOL in PHP is a platform from which employees and managers can use for collaboration and
+          communication.
+        </div>
+      </div>
+      <div id="socialBtnContainer">
+        
+        <a href="https://www.linkedin.com/in/pallab-mahata-a3b542246"><i class="fa-brands fa-linkedin"></i></a>
+      </div>
+      <div id="feedbackBtnContainer">
+        <button type="submit" id="feedbackBtn">Share your feedback!</button>
+      </div>
+    </div>
+
+    <div id="slide2">
+      <div id="feedbackFormContainer">
+        <button id="feedbackBackBtn">
+          <i class="fa-solid fa-backward"></i>
+        </button>
+        <div id="feedback">
+          <div id="title">Write your Feedback!</div>
+          <textarea id="feedbackContent" placeholder="Write here..."></textarea>
+          <button id="shareFeedback" type="submit">Share</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="developed">developed by @pallab</div>
+</footer>
