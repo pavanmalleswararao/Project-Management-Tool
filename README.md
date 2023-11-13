e26ed8d60d5e11c013e396939afa6e3cbe6f21e7
pavanmalleswararao <128814191+pavanmalleswararao@users.noreply.github.com>
2023-11-13 08:44:04 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project-Management-System
+Project Management TOOL includes a framework of social media sites where users can communicate with one another. The users also get the functionality to assign tasks to others. It is a platform from which employees and managers can use for collaboration and communication.
---
+++ pms_project.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 07, 2023 at 04:12 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pms_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `TID` varchar(20) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `task` varchar(256) NOT NULL,
+  `status` varchar(28) NOT NULL,
+  `UID` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`TID`, `username`, `task`, `status`, `UID`) VALUES
+('TID20230806103932681', 'Pallab Mahata', 'Remove bugs from navbar section.', 'In progress', 'UID20230806102830786'),
+('TID20230807040852006', 'Arnab Mahata', 'Remove extra spaces from menu bar.', 'Completed', 'UID20230807031646273');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `UID` varchar(20) NOT NULL,
+  `username` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UID`, `username`) VALUES
+('', 'HIII'),
+('UID20230806102830786', 'Pallab Mahata'),
+('UID20230806103756294', 'Pallab'),
+('UID20230807031646273', 'Arnab Mahata');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`TID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
